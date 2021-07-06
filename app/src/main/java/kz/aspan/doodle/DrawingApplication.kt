024b199c2aba268e772f8f128e3c6464bba878be
Samgar Tilek <aspansdu@gmail.com>
2021-07-06 20:27:38 +0600

    Dagger Hilt Setup

---
+++ app/src/main/java/kz/aspan/doodle/DrawingApplication.kt
@@ -0,0 +1,13 @@
+package kz.aspan.doodle
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class DrawingApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/kz/aspan/doodle/util/DispatcherProvider.kt
@@ -0,0 +1,9 @@
+package kz.aspan.doodle.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+}
