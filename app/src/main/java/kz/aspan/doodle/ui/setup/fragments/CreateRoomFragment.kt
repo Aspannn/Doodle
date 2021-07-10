7132334dc48c067c52e4b7a734c1b9f58e336b8b
Samgar Tilek <aspansdu@gmail.com>
2021-07-10 18:23:07 +0600

    Implemented CreateRoomFragment.kt

--- app/src/main/java/kz/aspan/doodle/ui/setup/fragments/CreateRoomFragment.kt
+++ app/src/main/java/kz/aspan/doodle/ui/setup/fragments/CreateRoomFragment.kt
@@ -3,16 +3,95 @@ package kz.aspan.doodle.ui.setup.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kz.aspan.doodle.R
+import kz.aspan.doodle.data.remote.ws.Room
 import kz.aspan.doodle.databinding.FragmentCreateRoomBinding
+import kz.aspan.doodle.ui.setup.SetupViewModel
+import kz.aspan.doodle.util.navigateSafely
+import kz.aspan.doodle.util.snackbar
 
-class CreateRoomFragment : Fragment() {
+@AndroidEntryPoint
+class CreateRoomFragment : Fragment(R.layout.fragment_create_room) {
     private var _binding: FragmentCreateRoomBinding? = null
     private val binding: FragmentCreateRoomBinding
         get() = _binding!!
 
+    private val viewModel: SetupViewModel by activityViewModels()
+    private val args: CreateRoomFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentCreateRoomBinding.bind(view)
+        setupRoomSizeSpinner()
+        listenRoEvents()
+
+        binding.btnCreateRoom.setOnClickListener {
+            binding.createRoomProgressBar.isVisible = true
+            viewModel.createRoom(
+                Room(
+                    binding.etRoomName.text.toString(),
+                    binding.tvMaxPerson.text.toString().toInt()
+                )
+            )
+        }
+    }
+
+    private fun listenRoEvents() {
+        lifecycleScope.launchWhenCreated {
+            viewModel.setupEvent.collect { event ->
+                when (event) {
+                    is SetupViewModel.SetupEvent.CreateRoomEvent -> {
+                        viewModel.joinRoom(args.username, event.room.name)
+                    }
+                    is SetupViewModel.SetupEvent.InputEmptyError -> {
+                        binding.createRoomProgressBar.isVisible = false
+                        snackbar(R.string.error_field_empty)
+                    }
+                    is SetupViewModel.SetupEvent.InputTooShortError -> {
+                        binding.createRoomProgressBar.isVisible = false
+                        snackbar(R.string.error_room_name_too_short)
+                    }
+                    is SetupViewModel.SetupEvent.InputTooLongError -> {
+                        binding.createRoomProgressBar.isVisible = false
+                        snackbar(R.string.error_room_name_too_long)
+                    }
+                    is SetupViewModel.SetupEvent.CreateRoomErrorEvent -> {
+                        binding.createRoomProgressBar.isVisible = false
+                        snackbar(event.error)
+                    }
+                    is SetupViewModel.SetupEvent.JoinRoomEvent -> {
+                        binding.createRoomProgressBar.isVisible = false
+                        findNavController().navigateSafely(
+                            R.id.action_createRoomFragment_to_drawingActivity,
+                            args = Bundle().apply {
+                                putString("username", args.username)
+                                putString("roomName", event.roomName)
+                            }
+                        )
+                    }
+                    is SetupViewModel.SetupEvent.JoinRoomErrorEvent -> {
+                        binding.createRoomProgressBar.isVisible = false
+                        snackbar(event.error)
+                    }
+                    else -> Unit
+                }
+
+            }
+        }
+    }
+
+    private fun setupRoomSizeSpinner() {
+        val roomSizes = resources.getStringArray(R.array.room_size_array)
+        val adapter = ArrayAdapter(requireContext(), R.layout.textview_room_size, roomSizes)
+        binding.tvMaxPerson.setAdapter(adapter)
     }
 
     override fun onDestroy() {
